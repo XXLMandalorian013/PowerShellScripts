9ee03ddb49494d3f7e718019c68547fd9a7931a1
Drew McClellan <d.mcclellan013@gmail.com>
2022-06-05 08:50:11 -0400

    Add files via upload

---
+++ LocalScripts/LocalScripts/Templates/BatToRunPSScripts.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""\\ServerName\Foler\995.ps1""' -Verb RunAs}"
