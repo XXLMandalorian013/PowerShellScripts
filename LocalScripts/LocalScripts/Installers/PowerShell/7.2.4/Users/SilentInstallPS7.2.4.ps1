68a8ff55b3f01abb2704c5b728066955a35b9ccc
Drew McClellan <d.mcclellan013@gmail.com>
2022-06-10 08:30:30 -0400

    Delete SilentInstallPS7.2.4.ps1

--- LocalScripts/LocalScripts/Installers/PowerShell/7.2.4/Users/SilentInstallPS7.2.4.ps1
+++
@@ -1,5 +0,0 @@
-#https://docs.microsoft.com/en-us/windows/package-manager/winget/install    
-    #Winget info
-        #https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?WT.mc_id=THOMASMAURER-blog-thmaure&view=powershell-7
-            #PS V 7.2.3.0
-            winget install --ID Microsoft.PowerShell --source winget --version 7.2.4.0 --accept-source-agreements --accept-package-agreements --silent --force
