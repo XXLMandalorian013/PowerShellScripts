59142366b37a0634365f154d7ac7e3c9f46b1afe
Drew McClellan <d.mcclellan013@gmail.com>
2022-07-05 14:32:35 -0400

    Delete LocalScripts/LocalScripts/Windows/OptionalFeatures/SMB1/EnableSM1 directory

--- LocalScripts/LocalScripts/Windows/OptionalFeatures/SMB1/EnableSM1/EnableSMB1.ps1
+++
@@ -1,5 +0,0 @@
-$SMB1 = Get-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol"  |Select-Object FeatureName, State, RestartRequired
-
-$SMB1 | Format-List
-
-Enable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol"
