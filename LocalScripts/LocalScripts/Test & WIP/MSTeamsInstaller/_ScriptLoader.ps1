1ff75ecfc4c41274e36301a24808190f21c42e23
Drew McClellan <d.mcclellan013@gmail.com>
2022-07-14 16:24:17 -0400

    Delete LocalScripts/LocalScripts/Test & WIP/MSTeamsInstaller directory

--- LocalScripts/LocalScripts/Test & WIP/MSTeamsInstaller/_ScriptLoader.ps1
+++
@@ -1,9 +0,0 @@
-$InstallerFolderLocation = Read-Host "Please type the dir path this script is saved at."
-
-pwsh.exe -command $InstallerFolderLocation\1_CreateATMPDirForDL.ps1
-
-pwsh.exe -command $InstallerFolderLocation\2_DownloadMSTeams.ps1
-
-pwsh.exe -command $InstallerFolderLocation\3_RunInstaller.ps1
-
-pwsh.exe -command $InstallerFolderLocation\4_DeletesTMPDLDir.ps1
