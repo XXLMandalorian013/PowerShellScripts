c011dd5327394cdd3d8f8c20ad423ff827d02098
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-07-25 15:03:59 -0400

    Update MobileDevice.ps1
    added some notes in the script and formatting

--- LocalScripts/LocalScripts/Installers/PSModules/Exchange/Scripts/MobileDevice/MobileDevice.ps1
+++ LocalScripts/LocalScripts/Installers/PSModules/Exchange/Scripts/MobileDevice/MobileDevice.ps1
@@ -1,3 +1,7 @@
-$Email = Read-Host "Type an email adress ie user@domain.com"
+#Gets Outlook Mobile info
 
-Get-EXOMobileDeviceStatistics -Mailbox $Email | Select-Object 'Status', 'ClientType', 'ClientVersion', 'DeviceFriendlyName', 'DeviceType', 'DeviceOS', 'LastSyncAttemptTime', 'LastSuccessSync' | Format-List
+    #https://docs.microsoft.com/en-us/powershell/module/exchange/get-exomobiledevicestatistics?view=exchange-ps
+
+        $Email = Read-Host "Type an email adress ie user@domain.com"
+
+        Get-EXOMobileDeviceStatistics -Mailbox $Email | Select-Object 'Status', 'ClientType', 'ClientVersion', 'DeviceFriendlyName', 'DeviceType', 'DeviceOS', 'LastSyncAttemptTime', 'LastSuccessSync' | Format-List
