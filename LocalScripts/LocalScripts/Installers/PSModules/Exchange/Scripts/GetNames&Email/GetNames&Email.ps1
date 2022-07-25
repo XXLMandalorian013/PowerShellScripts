e088b59be336be2a670c442e8ba07171b65c423f
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-07-25 15:06:20 -0400

    Update GetNames&Email.ps1
    added links and a description to script

--- LocalScripts/LocalScripts/Installers/PSModules/Exchange/Scripts/GetNames&Email/GetNames&Email.ps1
+++ LocalScripts/LocalScripts/Installers/PSModules/Exchange/Scripts/GetNames&Email/GetNames&Email.ps1
@@ -1,1 +1,5 @@
-Get-EXOMailbox | Select-Object 'DisplayName', 'UserPrincipalName', 'RecipientType' | Format-List
+#Gets Exchange email list and the users name.
+
+    #https://docs.microsoft.com/en-us/powershell/module/exchange/get-exomailbox?view=exchange-ps
+
+        Get-EXOMailbox | Select-Object 'DisplayName', 'UserPrincipalName', 'RecipientType' | Format-List
