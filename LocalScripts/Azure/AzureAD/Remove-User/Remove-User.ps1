e43f8413bdfe1222746e130e043475043ac5ef2d
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-08-17 12:38:25 -0400

    Update
    Fixed the removed AzureADUser script

--- LocalScripts/Azure/AzureAD/Remove-User/Remove-User.ps1
+++ LocalScripts/Azure/AzureAD/Remove-User/Remove-User.ps1
@@ -2,4 +2,8 @@
 
     #https://docs.microsoft.com/en-us/powershell/module/activedirectory/remove-aduser?view=windowsserver2022-ps
 
-        Remove-ADUser -Identity DirectoryNew -Confirm
+        Get-AzureADUser -Top 99999999
+
+        $ObjectID = Read-Host "Please type a User ObjectId to remove the user."
+
+        Remove-AzureADUser -ObjectId $ObjectID
