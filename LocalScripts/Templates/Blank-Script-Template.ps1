e43a2271fea6f79af0624c0b3f0a83647ed91181
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-08-18 12:21:37 -0400

    Update
    Added Blank-Script-Template.ps1

---
+++ LocalScripts/Templates/Blank-Script-Template.ps1
@@ -0,0 +1,39 @@
+<#
+        .SYNOPSIS
+        Adds a file name extension to a supplied name.
+
+        .DESCRIPTION
+        Adds a file name extension to a supplied name.
+        Takes any strings for the file name or extension.
+
+        .PARAMETER Name
+        Specifies the file name.
+
+        .PARAMETER Extension
+        Specifies the extension. "Txt" is the default.
+
+        .INPUTS
+        None. You cannot pipe objects to Add-Extension.
+
+        .OUTPUTS
+        System.String. Add-Extension returns a string with the extension or file name.
+
+        .EXAMPLE
+        PS> extension -name "File"
+        File.txt
+
+        .EXAMPLE
+        PS> extension -name "File" -extension "doc"
+        File.doc
+
+        .EXAMPLE
+        PS> extension "File" "doc"
+        File.doc
+
+        .LINK
+        Online version: http://www.fabrikam.com/extension.html
+
+        .LINK
+        Set-Item
+    #>
+
