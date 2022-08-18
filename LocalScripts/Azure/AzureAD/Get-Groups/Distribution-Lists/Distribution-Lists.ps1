f04b87a333e4450683b0154830bca2b83820b0e2
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-08-18 11:28:59 -0400

    Update
    Updated Readme.md

--- LocalScripts/Azure/AzureAD/Get-Groups/Distribution-Lists/Distribution-Lists.ps1
+++ LocalScripts/Azure/AzureAD/Get-Groups/Distribution-Lists/Distribution-Lists.ps1
@@ -1,4 +1,28 @@
-#Grabs a list of all distro list. In the MAC I have added Distribution-List as the description of all of our distro lists. Make sure to add that descript for every new distro list made.
+<#
+        .SYNOPSIS
+
+        Grabs a list of all distro list. 
+
+        .DESCRIPTION
+        
+        In the MAC I have added Distribution-List as the description of all of our distro lists. Make sure to add that descript for every new distro list made.
+
+        .OUTPUTS
+       
+        DisplayName             Description
+        -----------             -----------
+    1st Floor                 Distribution-List
+    All Mail List             Distribution-List
+    Autodesk Users            Distribution-List
+    iPad                      Distribution-List
+    Administrators            Distribution-List
+    Mobile Users              Distribution-List
+    Electrical                Distribution-List
+    Project Schedule Group    Distribution-List
+    BlueBeam Email list       Distribution-List
+
+    #>
+
     
     #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object?view=powershell-7.2
     
---
+++ LocalScripts/Azure/AzureAD/Get-Groups/Distribution-Lists/ReadMe.md
@@ -0,0 +1,25 @@
+# Get all distrobution lists in AzureAD.
+
+
+###        .SYNOPSIS
+
+##         Grabs a list of all distro list.
+
+
+##         .DESCRIPTION
+
+###         In the MAC I have added Distribution-List as the description of all of our distro lists. Make sure to add that descript for every new distro list made.
+
+
+###         DisplayName             Description
+###         -----------             -----------
+###     1st Floor                 Distribution-List
+###     All Mail List             Distribution-List
+###     Autodesk Users            Distribution-List
+###     iPad                      Distribution-List
+###     Administrators            Distribution-List
+###     Mobile Users              Distribution-List
+###     Electrical                Distribution-List
+###     Project Schedule Group    Distribution-List
+###     BlueBeam Email list       Distribution-List
+
