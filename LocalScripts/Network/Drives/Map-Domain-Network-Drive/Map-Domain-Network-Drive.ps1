c9b71cf1239b8f87b9d61a3fbbe62438227a69bc
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-08-22 18:18:45 -0400

    update

--- LocalScripts/Network/Drives/Map-Domain-Network-Drive/Map-Domain-Network-Drive.ps1
+++ LocalScripts/Network/Drives/Map-Domain-Network-Drive/Map-Domain-Network-Drive.ps1
@@ -16,6 +16,9 @@
         PowerShell credential request
         Enter your credentials.
         Password for user \DAM: ************
+        Name           Used (GB)     Free (GB) Provider      Root                                               CurrentLocation
+         ----           ---------     --------- --------      ----                                               ---------------
+         q                2672.54        119.21 FileSystem    \\Server\Folder
         
 
     .LINK
@@ -26,10 +29,14 @@
 
 #Script
 
+# Prejoined domain and domain profile added use only     $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain() | Select-Object 'Forest'
+
 $Domain = 'domain.com'
 
 $UserName = $env:UserName
 
 $Cred = Get-Credential -Credential $Domain\$UserName
 
-New-PSDrive -name 'Q' -psprovider 'FileSystem' -root '\\Server\Folder' -Credential $Cred -Persist
+$DriveLetter = Read-Host 'Enter driver letter you wish to map.'
+
+New-PSDrive -name "$DriveLetter" -psprovider 'FileSystem' -root '\\Server\Folder' -Credential $Cred -Persist
--- LocalScripts/Network/Drives/Map-Domain-Network-Drive/ReadMe.md
+++ LocalScripts/Network/Drives/Map-Domain-Network-Drive/ReadMe.md
@@ -8,7 +8,10 @@
 #        .OUTPUTS
 ##        PowerShell credential request
 ##        Enter your credentials.
-##        Password for user \DAM: ************
+##        Password for user \User: ************
+###         Name           Used (GB)     Free (GB) Provider      Root                                               CurrentLocation
+###         ----           ---------     --------- --------      ----                                               ---------------
+###          q                2672.54        119.21 FileSystem    \\Server\Folder
 
 #        .LINK
 ##         https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-psdrive?view=powershell-7.2
