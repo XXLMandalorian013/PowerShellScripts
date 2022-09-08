62fa906cb0c3c4adbd890a86c5082c5c5755179e
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-09-08 13:41:28 -0400

    Update
    added cmdlet

---
+++ LocalScripts/Templates/CMDlets/Get-ItemProperty/Get-ItemProperty.ps1
@@ -0,0 +1,14 @@
+    #ReadMe
+<#
+    
+Get-ItemProperty
+
+## Related Links
+
+* [OnlineVersion](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-itemproperty?view=powershell-7.2)
+
+#>
+
+#Script
+
+Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore'
