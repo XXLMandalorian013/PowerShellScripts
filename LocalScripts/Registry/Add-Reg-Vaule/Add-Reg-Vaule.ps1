30b7febbc4b5eebfa6f471b08018ccdc6b279f5f
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-09-16 14:32:20 -0400

    update
    moved Reg scripts

---
+++ LocalScripts/Registry/Add-Reg-Vaule/Add-Reg-Vaule.ps1
@@ -0,0 +1,17 @@
+    #ReadMe
+<#
+
+Name    
+    New-ItemProperty
+
+## Related Links
+
+[OnlineVersion] (https://docs.microsoft.com/en-us/powershell/module/Microsoft.PowerShell.Management/New-ItemProperty?view=powershell-7.2&viewFallbackFrom=powershell-3.0)
+
+#>
+
+#Script
+
+#Make sure to keep the Registry:: before the actual Reg key.
+
+New-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore' -Name  'SystemRestorePointCreationFrequency' -Value '0' -Type 'DWORD' –Force
