295d87b4d53a2ed4574a394e61dda412e405151e
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-10-04 08:32:06 -0400

    Update Enable-PS7Core-GPO.ps1

--- LocalScripts/PowerShell/PS7/Enable-PS7Core-GPO.ps1/Enable-PS7Core-GPO.ps1
+++ LocalScripts/PowerShell/PS7/Enable-PS7Core-GPO.ps1/Enable-PS7Core-GPO.ps1
@@ -52,8 +52,6 @@
 
 #To access "C:\Program Files\PowerShell\7\InstallPSCorePolicyDefinitions.ps1" the terminal needs elevated.
 
-Write-Host "Checking for elevated permissions..."
-
 if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
     Write-Error "This script requires Administrator rights. To run this cmdlet, start PowerShell with the `"Run as administrator`" option."
