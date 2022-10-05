ab13853255803176fc17865481a38656758b9de8
Drew <93335570+XXLMandalorian013@users.noreply.github.com>
2022-10-05 09:58:24 -0400

    update
    Added Exchange All-Users-Assigned-Roles

---
+++ LocalScripts/Exchange-Online/All-Users-Assigned-Roles/All-Users-Assigned-Roles.ps1
@@ -0,0 +1,52 @@
+    #ReadMe
+<#
+.DESCRIPTION
+        
+    Gets all Admin Users Exchange Roles
+    
+    
+.Notes
+
+    ExchangeOnlineManagment Module only workin in PS 7.X.X.
+    
+
+.INPUTS
+        
+    An Email of the Admin you wish to check.
+
+
+.OUTPUTS
+        
+    System.String.
+
+
+.EXAMPLE
+        
+    Role                                   RoleAssignee                   RoleAssigneeType
+    ----                                   ------------                   ----------------
+    Retention Management                   Organization Management        RoleGroup
+    My Custom Apps                         Organization Management        RoleGroup
+    Recipient Policies                     Organization Management        RoleGroup
+
+
+.LINK
+        
+    https://learn.microsoft.com/en-us/powershell/exchange/find-exchange-cmdlet-permissions?view=exchange-ps
+
+#>
+
+#Script
+
+param (
+    [Parameter(Mandatory,HelpMessage='Type the email you wish to check the roles for.')]
+    [string]$RoleAssignee)
+
+    $ParamArray = @(
+
+  "Role"
+  "RoleAssignee"
+  "RoleAssigneeType"
+
+)
+
+Get-ManagementRoleAssignment -RoleAssignee $RoleAssignee | Format-Table $ParamArray
---
+++ LocalScripts/Exchange-Online/All-Users-Assigned-Roles/ReadMe.md
@@ -0,0 +1,34 @@
+# All-Users-Assigned-Roles
+
+## DESCRIPTION
+
+Gets all Admin Users Exchange Roles
+
+## Notes
+
+ExchangeOnlineManagment Module only workin in PS 7.X.X.
+
+## INPUTS
+
+An Email of the Admin you wish to check.
+
+## OUTPUTS
+
+System.String.
+
+## EXAMPLE
+
+Role                                   RoleAssignee                   RoleAssigneeType
+
+----                                   ------------                   ----------------
+
+Retention Management                   Organization Management        RoleGroup
+
+My Custom Apps                         Organization Management        RoleGroup
+
+Recipient Policies                     Organization Management        RoleGroup
+
+
+# LINK
+
+[Find the permissions required to run any Exchange cmdlet](https://learn.microsoft.com/en-us/powershell/exchange/find-exchange-cmdlet-permissions?view=exchange-ps)
