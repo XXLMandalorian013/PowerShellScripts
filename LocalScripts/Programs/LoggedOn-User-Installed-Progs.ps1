50b6171442170ba5b175d8fdc3880c5e612a592f
Drew A. McClellan <d.mcclellan013@gmail.com>
2022-11-16 08:47:48 -0500

    update
    fixed machine wide installer

--- LocalScripts/Programs/LoggedOn-User-Installed-Progs.ps1
+++ LocalScripts/Programs/LoggedOn-User-Installed-Progs.ps1
@@ -1,4 +1,4 @@
-#Gets all installed programs from the resgistry.
+#Gets all logged in profile installed programs from the resgistry.
     #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-itemproperty?view=powershell-7.2
     $RegProg = Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object "DisplayName"
 
