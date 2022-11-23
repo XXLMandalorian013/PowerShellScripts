58e60ef0e161d5c19f9ed0fb88fe07881c66f7d9
Drew A. McClellan <d.mcclellan013@gmail.com>
2022-11-23 11:02:35 -0500

    update
    added mapped network drives

---
+++ LocalScripts/Network/Drives/Get-Network-Drives/Get-Network-Drives.ps1
@@ -0,0 +1,58 @@
+    #ReadMe
+<#
+    
+.SYNOPSIS
+
+Adds a file name extension to a supplied name.
+
+
+.DESCRIPTION
+        
+Adds a file name extension to a supplied name.  
+    
+    
+.Notes
+
+Notes here.
+    
+
+.PARAMETER Name
+        
+Specifies the file name.
+
+    
+.PARAMETER Extension
+        
+Specifies the extension. "Txt" is the default.
+
+
+.INPUTS
+        
+None. You cannot pipe objects to Add-Extension.
+
+
+.OUTPUTS
+        
+System.String. Add-Extension returns a string with the extension or file name.
+
+
+.EXAMPLE
+        
+PS> extension "File" "doc"
+File.doc
+
+
+.LINK
+        
+[WinGet Install Online Version](https://learn.microsoft.com/en-us/windows/package-manager/winget/install) 
+
+#>
+
+#Script
+
+$user = New-Object System.Security.Principal.NTAccount($env:UserName) 
+$sid = $user.Translate([System.Security.Principal.SecurityIdentifier]) 
+$SIDValue = $sid.Value
+
+Get-ChildItem -Path "Registry::HKEY_USERS\$SIDValue\Network" | Select-Object "Name"
+
