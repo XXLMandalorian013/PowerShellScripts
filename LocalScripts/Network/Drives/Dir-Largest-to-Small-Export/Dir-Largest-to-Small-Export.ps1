9a348fc29b760a1a88777ec97d794c51c3ac276d
Drew A. McClellan <d.mcclellan013@gmail.com>
2022-12-12 11:41:43 -0500

    Update
    Added a Get a Dir's folders largest to smallet and export that info to a .csv

---
+++ LocalScripts/Network/Drives/Dir-Largest-to-Small-Export/Dir-Largest-to-Small-Export.ps1
@@ -0,0 +1,46 @@
+    #ReadMe
+<#
+
+.DESCRIPTION
+        
+Gets all folders/files in a dir and sorts them largest to smallest and exports it to a .csv.   
+    
+.Notes
+
+Works in PS 5.1 and later.
+
+.INPUTS
+        
+None.
+
+.OUTPUTS
+        
+System.String.
+      
+
+.LINK
+        
+[Get-ChildItem Online Version](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-7.3) 
+
+[Sort-Object Online Version](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/sort-object?view=powershell-7.3)
+
+[Format-Table Online Version](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.3)
+
+[Export-CSV Online Version](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/export-csv?view=powershell-7.3)
+
+#>
+
+
+#Script
+
+$Server = '\ServerName\SharedDrive'
+
+$DirSize = foreach ($name in Get-ChildItem $Server -Recurse | Sort-Object -Property Length -Descending ) {
+    $name
+  }
+
+$ExportPath = "\\Server\Folder\Folder"
+
+$DirSize | Export-Csv -Path "$ExportPath\DriveSizeExport.csv"
+
+
