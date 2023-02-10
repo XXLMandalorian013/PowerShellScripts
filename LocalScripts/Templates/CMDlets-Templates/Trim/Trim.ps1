60a9751a97eb24a085bd0545cc98e5706fedb528
Drew McClellan <d.mcclellan013@gmail.com>
2023-02-10 10:09:51 -0500

    upload
    added Trim CMDLet

---
+++ LocalScripts/Templates/CMDlets-Templates/Trim/Trim.ps1
@@ -0,0 +1,29 @@
+    #ReadMe
+<#
+
+
+    .SYNOPSIS
+    
+        Trims characters off     
+#>  
+
+
+#Script
+
+
+#Trims out specific character
+
+$Server = '\\Server\Share'
+
+$Trim =  $Server.trim('\')
+
+$NameTrim = $Trim
+
+$NameTrim
+
+#Output of $NameTrim
+
+Server\Share
+
+
+
