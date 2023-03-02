c918e586ff61e21dc7f1e1588439ab44d1e7dc6f
Drew McClellan <d.mcclellan013@gmail.com>
2023-03-02 13:11:13 -0500

    Changed all scripts to specific script.

---
+++ LocalScripts/Programs/Get-Specific-Running-Program/Get-Specific-Running-Program.ps1
@@ -0,0 +1,69 @@
+    #ReadMe
+<#
+
+.DESCRIPTION
+        
+Gets a Specific running Program by prompting you.  
+    
+
+.INPUTS
+        
+System.String, ie
+
+Chrome
+
+
+.OUTPUTS
+        
+System.String,
+
+PS C:\Users\XXX> Get-SpecificProgram Chrome
+
+ NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+ ------    -----      -----     ------      --  -- -----------
+     64   156.31     240.26       1.66   47048   1 chrome
+
+.LINK
+
+[about_Functions](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions?view=powershell-7.3)
+
+[about_Functions_Advanced](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced?view=powershell-7.3)
+        
+[Get-Process](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-process?view=powershell-7.3)
+
+[Where-Object](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object?view=powershell-7.3)
+
+[about_Try_Catch_Finally](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_try_catch_finally?view=powershell-7.3)
+
+[about_If](https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-if?view=powershell-7.3)
+
+[Write-Error](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-error?view=powershell-7.3)
+
+
+#>
+
+#Script
+
+function Get-SpecificProgram {
+    
+    [CmdletBinding()]
+    param (
+    [Parameter(Mandatory,HelpMessage='Type the programs name ie Chrome')]
+    [string]$SingleProgram)
+
+    Try {
+
+        $RunningProgram = Get-Process | Where-object ProcessName -Match $SingleProgram -ErrorAction Stop
+
+        if ($RunningProgram) {
+            $RunningProgram | Where-Object { $_.MainWindowTitle }
+        }else {
+            Write-Host "$SingleProgram is not running"
+        }
+
+    }Catch{
+    Write-Error -Message $_
+    }  
+}
+
+Get-SpecificProgram
