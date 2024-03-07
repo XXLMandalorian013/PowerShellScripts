d9f6231778c088787a0d71cba6791dbfa72f4004
Drew McClellan <d.mcclellan013@gmail.com>
2024-03-07 14:11:29 -0500

    Dice Dungeon game start

---
+++ LocalScripts/Games/Dice-Adventure/Monsters/Goblin.ps1
@@ -0,0 +1,11 @@
+
+$GoblinSkillChart = @{
+    Strength = '10'
+    Dexterity   = '13'
+    Constitution  = '5'
+    Intelligence = '12'
+    Wisdom = '13'
+    charisma = '15'
+}
+$GoblinSkillCheck = $GoblinSkillChart
+
