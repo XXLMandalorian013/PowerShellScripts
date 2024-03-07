2e24725054159c7fbc62417cce609762293e8912
Drew McClellan <d.mcclellan013@gmail.com>
2024-03-07 14:12:01 -0500

    Dice Dungeon update

---
+++ LocalScripts/Games/Dice-Dungeon/Weapons/Bow-and-Arrows.ps1
@@ -0,0 +1,10 @@
+function BowAndArrows {
+    $BowAndArrows = @(
+    '2'
+    '3'
+    '4'
+)
+$BowandArrows | Get-Random
+}
+BowAndArrows
+
---
+++ LocalScripts/Games/Dice-Dungeon/Weapons/Dagger.ps1
@@ -0,0 +1,10 @@
+function Dagger {
+    $Dagger = @(
+    '1'
+    '2'
+    '3'
+)
+$Dagger | Get-Random
+}
+Dagger
+
