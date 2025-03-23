9e23f7f4f00041e7c299a7f37d6c57d5355ffda8
Drew McClellan <d.mcclellan013@gmail.com>
2025-03-23 16:42:25 -0400

    last 5 PW events updated

--- LocalScripts/Event-Log/Reboots/Power-Events/Last-5-Power-Events.ps1
+++ LocalScripts/Event-Log/Reboots/Power-Events/Last-5-Power-Events.ps1
@@ -1,10 +1,5 @@
-Write-Verbose -Message "Outputting the last 5 events on power events" -Verbose
-# Define the event IDs related to power events
-$powerEventIDs = @(5354)
-# Query the System log for these event IDs and get the last 5 events
-Get-WinEvent -LogName System | Where-Object { $powerEventIDs -contains $_.Id } | Select-Object -Last 5 | Format-List
-Write-Verbose -Message "Outputting the last 5 events on PSU Issues" -Verbose
-# Define the keyword to search for
-$keyword = "PSU"
-# Query the System log for events containing the keyword
-Get-WinEvent -LogName System | Where-Object { $_.Message -like "*$keyword*" } | Select-Object -Last 5 | Format-List
+Write-Verbose -Message "Getting the last 5 Power Events...please wait" -verbose
+# Define the event IDs to filter
+$eventIDs = @(41, 1074, 6006, 6008)
+# Get the last 10 events for the specified event IDs
+Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID } | Select-Object -Property TimeGenerated, EntryType, Source, EventID, Message -Last 5
