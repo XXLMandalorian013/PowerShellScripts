154b75ccfba16b56f3b9574ec8f359bf9dc47942
Drew McClellan <d.mcclellan013@gmail.com>
2025-04-07 10:59:04 -0400

    Update Last-10-Reboots.ps1

--- LocalScripts/Event-Log/Reboots/Last-10-Reboots/Last-10-Reboots.ps1
+++ LocalScripts/Event-Log/Reboots/Last-10-Reboots/Last-10-Reboots.ps1
@@ -1,5 +1,5 @@
-Write-Verbose -Message "Getting the last 10 reboot...please wait" -verbose
-# Define the event IDs to filter
+# Define the event IDs related to system reboots
 $eventIDs = @(41, 1074, 6006, 6008)
-# Get the last 10 events for the specified event IDs
-Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID } | Select-Object -Property TimeGenerated, EntryType, Source, EventID, Message -Last 10
+# Get the last 10 events matching the specified event IDs
+Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID } | 
+Select-Object -First 10 TimeGenerated, EntryType, Source, EventID, Message
