08fa6c83e128502f71258047dcf17d2c583cb0e3
Drew McClellan <d.mcclellan013@gmail.com>
2025-04-07 11:00:40 -0400

    Update Get-Last-Reboot.ps1

--- LocalScripts/Computer/Get-Last-Reboot/Get-Last-Reboot.ps1
+++ LocalScripts/Computer/Get-Last-Reboot/Get-Last-Reboot.ps1
@@ -1,1 +1,6 @@
-Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object LastBootUpTime
+Get-BitlockerVolume
+# Define the event IDs related to system reboots
+$eventIDs = @(41, 1074, 6006, 6008)
+# Get the last 10 events matching the specified event IDs
+Get-EventLog -LogName System | Where-Object { $eventIDs -contains $_.EventID } | 
+Select-Object -First 1 TimeGenerated, EntryType, Source, EventID, Message
