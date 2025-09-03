76b5ab52033b73d56257d5968cebf370cbb2c9da
Drew McClellan <d.mcclellan013@gmail.com>
2025-09-03 11:26:52 -0400

    Update CCC-ZAC-Uninstaller.ps1

--- LocalScripts/Software/Uninstall/CCC-ZAC-Uninstaller/CCC-ZAC-Uninstaller.ps1
+++ LocalScripts/Software/Uninstall/CCC-ZAC-Uninstaller/CCC-ZAC-Uninstaller.ps1
@@ -1,66 +1,3 @@
-#ReadMe
-<#
-Cloud-Contact-Center-ZAC-Uninstaller.ps1
-
-.SYNOPSIS
-Downloads and installs Windows MSI WebRoot Agent Installer if not already installed. It also will make sure another AV is not already installed.
-That AV must be defined in the Install-WebRoot finction paramater.
-
-.Notes
-
-The uninstaller requires the same .exe installer to be used to uninstall the current version.
-
-.INPUTS
-
-None.
-
-.OUTPUTS
-
-System.String,
-
-Start-ScriptBoilerPlate
-VERBOSE: Cloud-Contact-Center-ZAC-Installer.ps1 Written by DAM on 2024-03-05
-
-Uninstall-CCCZac
-VERBOSE: Downloading uninstaller ZAC_x86-8.4.34.exe...
-VERBOSE: Uninstalling ZAC_x86-8.4.34.exe
-VERBOSE: ZAC_x86-8.4.34.exe uninstalled!
-VERBOSE: ZAC_x86-8.4.34.exe removed
-
-or
-
-VERBOSE: ZAC_x86-8.4.34.exe is already uninstalled...
-
-.LINK
-
-[Write-Verbose](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-verbose?view=powershell-7.3)
-
-[about_If](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_if?view=powershell-7.3)
-
-[about_Functions](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions?view=powershell-7.3)
-
-[about_Try_Catch_Finally](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_try_catch_finally?view=powershell-7.3)
-
-[Approved Verbs for PowerShell Commands](https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands?view=powershell-7.3)
-
-[about_Operators](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.3)
-
-[about_Throw](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_throw?view=powershell-7.3)
-
-[Invoke-WebRequest](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-7.3)
-
-[msiexec](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/msiexec)
-
-[about_Do](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_do?view=powershell-7.3)
-
-[Remove-Item](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/remove-item?view=powershell-7.3)
-
-[Accent's Cloud-Contact-Center-ZAC-Installer download URL](https://www.accentvoice.com/downloads/)
-
-#>
-
-#Script
-
 #Checks if the terminal is runing as admin/elevated as Invoke-WebRequest will not run without it.
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     
@@ -71,38 +8,41 @@ function Start-ScriptBoilerPlate {
     [CmdletBinding()]
     param (
         #Script Name
-        $ScriptName = 'Cloud-Contact-Center-ZAC-Uninstaller.ps1',
+        $ScriptName = 'Zac-Uninstaller.ps1',
         #Script Author
-        $ScriptAuthor = 'Written by DAM on 2024-03-05'
+        $ScriptAuthor = 'Written by DAM on 2025-09-03'
     )
     #Script Introduction
     Write-Verbose -Message "$ScriptName $ScriptAuthor" -Verbose 
 }
-#Installs Accent's Cloud Contact Center ZAC
-function Uninstall-CCCZac {
-    param(
-        #Program Path when its installed.
-        $ProgramPath = 'C:\Program Files (x86)\Zultys\ZAC',
-        #Download Link, cut off to have the installed version amened on the path.
-        $URI = 'https://mirror.zultys.biz/ZAC',
-        #Out-File location.
-        $OutFile = "C:"
+#Tries to uninstall zac.exe 
+function Uninstall-Zac {
+    param (
+        #Vars
+    #Program Path when its installed.
+    $ProgramPath = 'C:\Program Files (x86)\Zultys\ZAC\zac.exe',
+    #Download Link, cut off to have the installed version amened on the path.
+    $URI = 'https://mirror.zultys.biz/ZAC',
+    #Out-File location.
+    $OutFile = "C:"
     )
+    #Tries to uninstall zac.exe.    
     $ExsistingInsatll = Test-Path -Path "$ProgramPath"
-    if ($ExsistingInsatll -match 'False') {
-        Write-Verbose -Message "$InstallerName is already uninstalled..." -Verbose
-    }else {
-        #Downloads the required exe.
-        try {
-            #Grabs the installed version as the uninstaller only works with the same installed version for cli uninstalls.
-            Write-Verbose -Message "Grabbing installed version to DL the right uninstaller version..." -Verbose
-            (Get-Item "C:\Program Files (x86)\Zultys\ZAC\zac.exe").VersionInfo.FileVersion
-        }catch {
-            Write-Verbose -Message"$Error[0]" -Verbose
+        if ($ExsistingInsatll -match 'False') {
+            Write-Verbose -Message "$InstallerName is already uninstalled..." -Verbose
+        }else {
+            try {
+                #Grabs the installed version as the uninstaller only works with the same installed version for cli uninstalls.
+                Write-Verbose -Message "Grabbing installed version to DL the right uninstaller version..." -Verbose
+                (Get-Item "C:\Program Files (x86)\Zultys\ZAC\zac.exe").VersionInfo.FileVersion
+            }catch {
+                Write-Verbose -Message"$Error[0]" -Verbose
+            }
         }
         #Downloads the required exe.
         try {
-            #
+            # TLS 1.2 ensured for download.
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
             $InstalledVersion = (Get-Item "C:\Program Files (x86)\Zultys\ZAC\zac.exe").VersionInfo.FileVersion
             #Disabled Invove-WebReqests progress bar speeding up the download. Bug seen here https://github.com/PowerShell/PowerShell/issues/2138
             $ProgressPreference = 'SilentlyContinue'
@@ -113,15 +53,22 @@ function Uninstall-CCCZac {
             Write-Verbose -Message"$InstallerName may not match what is being download anymore?" -Verbose
             Write-Verbose -Message"Invoke-WebRequest is not supported on Win 7..." -Verbose
         }
-        #Installs the Program from URI.
+        #Kills the running program.
         try {
-             Write-Verbose -Message "Uninstalling $InstallerName" -Verbose
+            Write-Verbose -Message "Uninstalling $InstallerName" -Verbose
+            taskkill /f /im zac.exe
+            Start-Sleep -Seconds 8
+        }catch {
+            Write-Verbose -Message "$Error[0]" -Verbose
+        }
             #Uninstalls the program, per the installer.
-            $InstallerPath = "C:\ZAC_x64-9.4.11.exe"
+        try {
+            Write-Verbose -Message "Uninstalling $InstallerName" -Verbose
+            $InstallerPath = "$OutFile\ZAC_x64-$InstalledVersion.exe"
             $Arguments = "/x /s /v/qn"
             Start-Process -FilePath $InstallerPath -ArgumentList $Arguments
             #Give the uninstaller a second
-            Start-Sleep -Seconds 30
+            Start-Sleep -Seconds 20
         }catch {
             Write-Verbose -Message "$Error[0]" -Verbose
         }
@@ -130,20 +77,19 @@ function Uninstall-CCCZac {
             do { 
                 $TestPath = Test-Path -Path "$ProgramPath"
                 if ($TestPath -match 'True') {
-                    Write-Verbose -Message "$InstallerName uninstaller is running...Please wait" -Verbose
-                    Start-Sleep -Seconds 5
-                }
-            }Until ($TestPath -match 'False')
-                Write-Verbose -Message "$InstallerName uninstalled!" -Verbose
-                Start-Sleep -Seconds 20
-                Write-Verbose -Message "$InstallerName removed" -Verbose
-                Remove-Item "$OutFile"
-        }catch {
-            Write-Verbose -Message "$Error[0]" -Verbose
-        }
+                Write-Verbose -Message "$InstallerName uninstaller is running...Please wait" -Verbose
+                Start-Sleep -Seconds 5
+            }
+        }Until ($TestPath -match 'False')
+            Write-Verbose -Message "$InstallerName uninstalled!" -Verbose
+            Start-Sleep -Seconds 10
+            Write-Verbose -Message "$InstallerName removed" -Verbose
+            Remove-Item "$OutFile"
+    }catch {
+        Write-Verbose -Message "$Error[0]" -Verbose
     }
 }
-
+#Script Boilerplate
 Start-ScriptBoilerPlate
-
-Uninstall-CCCZac
+#Tries to uninstall zac.exe 
+Uninstall-Zac
