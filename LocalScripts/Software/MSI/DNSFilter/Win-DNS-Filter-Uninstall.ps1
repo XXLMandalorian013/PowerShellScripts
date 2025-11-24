8b584259757b611f724d7b7502b97a1e483d6533
Drew McClaren <d.mcclellan013@gmail.com>
2025-11-24 12:16:42 -0500

    Refactor DNSFilter uninstall script for clarity
    Removed redundant uninstallation steps and added comments for clarity.

--- LocalScripts/Software/MSI/DNSFilter/Win-DNS-Filter-Uninstall.ps1
+++ LocalScripts/Software/MSI/DNSFilter/Win-DNS-Filter-Uninstall.ps1
@@ -1,13 +1,17 @@
+#This script is for the standard roaming agent uninstall and not the MPS/White label version.
+#Script
+Write-verbose -Message "Checking the registry for DNSFilter...$RegValue" -Verbose
+#Gets the registry value for the DNSFilter Agent.
 $RegValue = Get-ItemProperty -Path "HKLM:\SOFTWARE\DNSFilter\Agent"
-
-Write-verbose -Message "Grabbing the devices ProductGUID...$RegValue" -Verbose
-
+#Gets the product GUID from the registry.
 $GUID = $RegValue.ProductGUID
+if (-not $GUID) {
+    throw "Can't quarry the registry for the product GUID...DNSFilter Agent does not seem to be installed...ending script..."
+}else {
+    Write-verbose -Message "GUID found...$GUID...Running the uninstaller..." -Verbose
+    msiexec /X{$GUID} REGCLEAN=true
+    Write-verbose -Message "Waiting 60 seconds for the uninstall to complete..." -Verbose
+    start-sleep -seconds 60
+    Write-verbose -Message "a reboot is required to finish the install...ending script..." -Verbose
+}
 
-Write-verbose -Message "Running the uninstaller...$GUID" -Verbose
-
-msiexec /X{$GUID} REGCLEAN=true
-
-start-sleep -seconds 30
-
-Write-verbose -Message "a reboot is required to finish the install...ending script..." -Verbose
